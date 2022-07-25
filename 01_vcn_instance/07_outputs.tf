d4a0f35e2a3b66cc541d0b7479bad2feb6d99fb3
christophe.pauliat@oracle.com <christophe.pauliat@oracle.com>
2022-07-25 15:21:10 +0200

    fix names in outputs

--- 01_vcn_instance/07_outputs.tf
+++ 01_vcn_instance/07_outputs.tf
@@ -2,16 +2,9 @@
 output Instances {
   value = <<EOF
 
-  ---- The IP addresses assigned to the bastion instance are : 
-  private IP: ${oci_core_instance.tf-demo01-bastion.private_ip}
-  public IP : ${oci_core_instance.tf-demo01-bastion.public_ip}
-  
-  ---- The IP address assigned to the private instance is : 
-  private IP: ${oci_core_instance.tf-demo01-private.private_ip}
-
   ---- You can SSH to the 2 instances using the following commands:
-    ssh -F sshcfg d02bastion
-    ssh -F sshcfg d02private
+    ssh -F sshcfg d01bastion
+    ssh -F sshcfg d01private
 
 EOF
 }
@@ -19,17 +12,17 @@ EOF
 # ------ Create a SSH config file
 resource local_file sshconfig {
   content = <<EOF
-Host d02bastion
+Host d01bastion
           Hostname ${oci_core_instance.tf-demo01-bastion.public_ip}
           User opc
           IdentityFile ${var.ssh_private_key_file_bastion}  
           StrictHostKeyChecking no
-Host d02private
+Host d01private
           Hostname ${oci_core_instance.tf-demo01-private.private_ip}
           User opc
           IdentityFile ${var.ssh_private_key_file_private}  
           StrictHostKeyChecking no
-          proxycommand /usr/bin/ssh -F sshcfg -W %h:%p d02bastion
+          proxycommand /usr/bin/ssh -F sshcfg -W %h:%p d01bastion
 EOF
 
   filename = "sshcfg"
